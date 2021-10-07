e2e7103123d87cc18d2cdce88b07d5efb9770345
Hyun <pinkiepies0912@gmail.com>
2021-10-07 22:38:36 +0900

    feature: Comment out unused tests

--- app/trading/tests/bitcoin/test_history_reader_repository.py
+++ app/trading/tests/bitcoin/test_history_reader_repository.py
@@ -1,55 +1,55 @@
-import datetime
-
-from pinkiepie_trading.coin.models.bitcoin import BitCoin
-from pinkiepie_trading.coin.repositories.history_reader import (
-    BitcoinHistoryReaderRepository,
-)
-
-
-def test_get_by(session, bitcoin_factory):
-    # given
-    target_date = datetime.datetime.now().date()
-    bitcoin_factory(
-        price=50643.1,
-        open=48,
-        high=899,
-        low=51,
-        volume=111,
-        change=48,
-        date=target_date,
-    )
-
-    repository = BitcoinHistoryReaderRepository(session=session)
-
-    # when
-    bitcoin = repository.get_by(target_date)
-
-    # then
-    assert isinstance(bitcoin, BitCoin)
-
-
-def test_get_list_by(session, bitcoin_factory):
-    # given
-    history_count = 10
-
-    end_date = datetime.datetime.now().date()
-    start_date = end_date - datetime.timedelta(days=history_count)
-
-    for day in range(history_count):
-        bitcoin_factory(
-            price=50643.1,
-            open=48,
-            high=899,
-            low=51,
-            volume=111,
-            change=48,
-            date=start_date + datetime.timedelta(days=day),
-        )
-
-    repository = BitcoinHistoryReaderRepository(session=session)
-
-    # when
-    history = repository.get_list_by(start_date, end_date)
-
-    # then
-    assert len(history) == history_count
+# import datetime
+#
+# from pinkiepie_trading.coin.models.bitcoin import BitCoin
+# from pinkiepie_trading.coin.repositories.history_reader import (
+#     BitcoinHistoryReaderRepository,
+# )
+#
+#
+# def test_get_by(session, bitcoin_factory):
+#     # given
+#     target_date = datetime.datetime.now().date()
+#     bitcoin_factory(
+#         price=50643.1,
+#         open=48,
+#         high=899,
+#         low=51,
+#         volume=111,
+#         change=48,
+#         date=target_date,
+#     )
+#
+#     repository = BitcoinHistoryReaderRepository(session=session)
+#
+#     # when
+#     bitcoin = repository.get_by(target_date)
+#
+#     # then
+#     assert isinstance(bitcoin, BitCoin)
+#
+#
+# def test_get_list_by(session, bitcoin_factory):
+#     # given
+#     history_count = 10
+#
+#     end_date = datetime.datetime.now().date()
+#     start_date = end_date - datetime.timedelta(days=history_count)
+#
+#     for day in range(history_count):
+#         bitcoin_factory(
+#             price=50643.1,
+#             open=48,
+#             high=899,
+#             low=51,
+#             volume=111,
+#             change=48,
+#             date=start_date + datetime.timedelta(days=day),
+#         )
+#
+#     repository = BitcoinHistoryReaderRepository(session=session)
+#
+#     # when
+#     history = repository.get_list_by(start_date, end_date)
+#
+#     # then
+#     assert len(history) == history_count
--- app/trading/tests/bitcoin/test_model.py
+++ app/trading/tests/bitcoin/test_model.py
@@ -1,29 +1,29 @@
-import datetime
-
-from trading_db.rdb.bitcoin import Bitcoin as SABitcoin
-
-from pinkiepie_trading.coin.models.bitcoin import BitCoin
-
-
-def test_of():
-    # given
-    sa_obj = SABitcoin(
-        price=50643.1,
-        open=48,
-        high=899,
-        low=51,
-        volume=111,
-        change=48,
-        date=datetime.datetime.now().date,
-    )
-
-    # when
-    bitcoin = BitCoin.of(sa_obj)
-
-    # then
-    assert bitcoin.price == sa_obj.price
-    assert bitcoin.open == sa_obj.open
-    assert bitcoin.high == sa_obj.high
-    assert bitcoin.volume == sa_obj.volume
-    assert bitcoin.change == sa_obj.change
-    assert bitcoin.date == sa_obj.date
+# import datetime
+#
+# from trading_db.rdb.bitcoin import Bitcoin as SABitcoin
+#
+# from pinkiepie_trading.coin.models.bitcoin import BitCoin
+#
+#
+# def test_of():
+#     # given
+#     sa_obj = SABitcoin(
+#         price=50643.1,
+#         open=48,
+#         high=899,
+#         low=51,
+#         volume=111,
+#         change=48,
+#         date=datetime.datetime.now().date,
+#     )
+#
+#     # when
+#     bitcoin = BitCoin.of(sa_obj)
+#
+#     # then
+#     assert bitcoin.price == sa_obj.price
+#     assert bitcoin.open == sa_obj.open
+#     assert bitcoin.high == sa_obj.high
+#     assert bitcoin.volume == sa_obj.volume
+#     assert bitcoin.change == sa_obj.change
+#     assert bitcoin.date == sa_obj.date
