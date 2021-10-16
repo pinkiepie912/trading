d01e0483c51a03a715f9b6cafb39ef37a1b8f3aa
Hyun <pinkiepies0912@gmail.com>
2021-10-16 21:27:56 +0900

    feature: Add health router

--- app/trading/pinkiepie_trading/app.py
+++ app/trading/pinkiepie_trading/app.py
@@ -1,4 +1,4 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, status
 
 from .config import Config
 from .database import db
@@ -8,6 +8,11 @@ from .stock.controllers import stock_firm_router
 def create_app(config: Config) -> FastAPI:
     # Create FastAPI app.
     app = FastAPI()
+
+    # Health check
+    @app.get("/health", status_code=status.HTTP_200_OK)
+    async def health():
+        return {"status": "ok"}
 
     # Add routers
     app.include_router(stock_firm_router)
