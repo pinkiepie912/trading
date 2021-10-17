a027e614fa194da423be47490078691b79a74f8b
Hyun <pinkiepies0912@gmail.com>
2021-10-17 12:35:12 +0900

    feature: register ticker router

--- app/trading/pinkiepie_trading/app.py
+++ app/trading/pinkiepie_trading/app.py
@@ -2,7 +2,7 @@ from fastapi import FastAPI, status
 
 from .config import Config
 from .database import db
-from .stock.controllers import stock_firm_router
+from .stock.controllers import stock_firm_router, stock_ticker_router
 
 
 def create_app(config: Config) -> FastAPI:
@@ -16,6 +16,7 @@ def create_app(config: Config) -> FastAPI:
 
     # Add routers
     app.include_router(stock_firm_router)
+    app.include_router(stock_ticker_router)
 
     # DB initializing
     db.init_app(app, config=config.TRADING_SQLALCHEMY_DATABASE)
--- app/trading/pinkiepie_trading/stock/controllers/__init__.py
+++ app/trading/pinkiepie_trading/stock/controllers/__init__.py
@@ -1,3 +1,4 @@
 from .stock_firm import stock_firm_router
+from .stock_ticker import stock_ticker_router
 
-__all__ = ("stock_firm_router",)
+__all__ = ("stock_firm_router", "stock_ticker_router")
